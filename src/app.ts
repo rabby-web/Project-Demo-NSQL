228105d200d5e8b37bc6c3c6625ff2ac6707c6d5
rabby-web <rabby.webdeveloper@gmail.com>
2024-09-07 15:04:26 +0600

    server and config add

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,8 @@
 import express, { Request, Response } from "express";
 const app = express();
-const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World 😂");
+  res.send("Hello World Demo😂");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+export default app;
